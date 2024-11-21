5ee65cb4411883ec13003310b6613b2c01d228c0
Benjamin Koller <bkoller@isengard.mines.edu>
2024-11-21 13:40:57 -0700

    nov21-5

--- server.c
+++ server.c
@@ -35,7 +35,7 @@ int main() {
 		read(server,&req,sizeof(req));
 		}
 
-		// printf("Received a request from %s to send the message %s to %s.\n",req.source,req.msg,req.target);
+		 printf("Received a request from %s to send the message %s to %s.\n",req.source,req.msg,req.target);
 
 		// TODO:
 		// open target FIFO and write the whole message struct to the target FIFO
